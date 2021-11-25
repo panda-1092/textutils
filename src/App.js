74d071855c8340aecd8587438adc3cc8ea0b58c1
panda-1092 <pandajanmejaya2000@gmail.com>
2021-11-25 12:41:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 
 import React, { useState } from 'react';
 
